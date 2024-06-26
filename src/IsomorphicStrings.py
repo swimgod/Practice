9c10fcc79bff2da3555ff85f035c36aab109ecf3
Gary Fong <garyj.fong@gmail.com>
2024-06-26 16:43:08 -0700

    hashmap solution top isomorphic strings

--- src/IsomorphicStrings.py
+++ src/IsomorphicStrings.py
@@ -5,26 +5,21 @@ from itertools import groupby
 class IsomorphicStrings:
     @staticmethod
     def isIsomorphic(s: str, t: str) -> bool:
-        s_group = [(i, len(list(j))) for i, j in groupby(s)]
-        t_group = [(i, len(list(j))) for i, j in groupby(t)]
-        print(s_group)
-        print(t_group)
-        if len(s_group) != len(t_group):
-            return False
+        char_dict = {}
+        char_set = set()
 
-        s_index_count = 0
-        t_index_count = 0
-        for a, b in zip(s_group, t_group):
-            s_group_count = a[1]
-            t_group_count = b[1]
+        for i in range(len(s)):
+            s_char = s[i]
+            t_char = t[i]
 
-            if s_group_count != t_group_count:
-                return False
-
-            full_s_group = s[s_index_count:s_index_count + s_group_count]
-            full_t_group = t[t_index_count:t_index_count + t_group_count]
-            s_index_count += s_group_count
-            t_index_count += t_group_count
-            print(full_s_group)
-            print(full_t_group)
+            if s_char not in char_dict:
+                if t_char in char_set:
+                    return False
+                char_dict[s_char] = t_char
+                char_set.add(t_char)
+            else:
+                if t_char != char_dict[s_char]:
+                    return False
         return True
+
+
