ace9f509a95040dd7917fae2a9083495bd1adccd
Gary Fong <garyj.fong@gmail.com>
2024-07-16 15:52:28 -0700

    Alluxio tech interview

--- src/tech_interviews/Alluxio.py
+++ src/tech_interviews/Alluxio.py
@@ -3,16 +3,6 @@
 # Return the number of instances for each word
 # Return the words with the highest count
 
-def unique_words(sentence: str) -> [str]:
-    words = set()
-    sentence = sentence.lower().split(' ')
-    for word in sentence:
-        if word not in words:
-            words.add(word)
-
-    return list(words)
-
-
 def unique_words_count(sentence: str) -> dict:
     word_dict = {}
     for word in sentence.lower().split(' '):
@@ -24,7 +14,14 @@ def unique_words_count(sentence: str) -> dict:
     return word_dict
 
 
+def highest_count_words(word_dict: dict) -> []:
+    max_count = max(word_dict.values())
+    return [x for x in word_dict if word_dict[x] >= max_count]
+
+
 if __name__ == "__main__":
     input_string = "Wait at the bus stop at Manhattan Beach for a bus to Mount Saint Helens"
-    print(unique_words(input_string))
-    print(unique_words_count(input_string))
+    unique_words_dict = unique_words_count(input_string)
+    print(sorted(list(unique_words_dict.keys())))
+    print(unique_words_dict)
+    print(highest_count_words(unique_words_dict))
